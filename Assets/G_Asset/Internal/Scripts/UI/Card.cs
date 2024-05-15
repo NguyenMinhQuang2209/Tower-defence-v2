cafa336a1cbc57b6f42d64ec876593aadd8ec425
Quang Minh <quangnm2209@gmail.com>
2024-05-15 08:13:29 +0700

    adding card UI

---
+++ Assets/G_Asset/Internal/Scripts/UI/Card.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TextMeshProUGUI itemName;
+    [SerializeField] private Button useBtn;
+}
